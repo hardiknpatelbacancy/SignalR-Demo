c3a39e1617edfc76f3666fd8245bd1725f8d6613
hardiknpatel <hardik.n.patel@bacancy.com>
2024-12-13 14:52:12 +0530

    init

---
+++ WebApplicationSignalRSample/signalR/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace WebApplicationSignalRSample.signalR
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
